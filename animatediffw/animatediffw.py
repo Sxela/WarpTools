92f2ef32d4dfe1b4f2d470714665f127495dd719
Sxela <defiler.off@gmail.com>
2023-10-21 08:58:24 +0700

    add sched

--- animatediffw/animatediffw.py
+++ animatediffw/animatediffw.py
@@ -1,5 +1,8 @@
 # (c) Alex Spirin 2022-2023
 
+import math
+import random
+
 #inspired by https://github.com/ArtVentureX/comfyui-animatediff/blob/main/animatediff/sampler.py
 def inject_motion_module_to_unet(diffusion_model, mm):
 
@@ -15,5 +18,42 @@ def inject_motion_module_to_unet(diffusion_model, mm):
             diffusion_model.output_blocks[i].append(mm.up_blocks[a].motion_modules[b])
 
     diffusion_model.middle_block.insert(-1, mm.mid_block.motion_modules[0])
+
+def make_ctx_sched(
+    total_length = 32,
+    context_length = 16,
+    overlap = 4,
+    steps = 15,
+    shuffle = True
+):
+
+    idxs=list(range(total_length))
+    step = context_length-overlap
+    step_ids = []
+
+    for i in range(steps):
+        """add 1st and last passes"""
+        inner_ids = [idxs[:context_length]]
+        if idxs[-context_length:] not in inner_ids:
+            inner_ids.append(idxs[-context_length:])
+
+        """change offset from 0 to step in increments of 2"""
+        start_offset = max(-step, -((i)*2%step))
+        for j in range(math.ceil(len(idxs)/(step))):
+
+            start = j*step+start_offset
+            end = j*step + context_length+start_offset
+            
+            if end>len(idxs):
+                """drop last or overshooting ids as we already have last pass"""
+                continue
+
+            ids = idxs[start:end]
+            if ids not in inner_ids and ids!=[]:
+                """drop duplicates"""
+                inner_ids.append(ids)
+        if shuffle: random.shuffle(inner_ids)
+        step_ids.append(inner_ids)
+    return step_ids
 
 
